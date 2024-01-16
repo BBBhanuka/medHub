5d554a0d72a76cf44a9d8cc3ea0cb6dc9b29ce77
BBBhanuka <bbbhanuka@gmai.com>
2024-01-16 15:17:25 +0530

    added new button and lables for med count etc

--- app/src/main/java/com/example/healthcare/BuyMedicineBookActivity.java
+++ app/src/main/java/com/example/healthcare/BuyMedicineBookActivity.java
@@ -7,6 +7,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -15,9 +17,9 @@ import android.widget.Toast;
 
 public class BuyMedicineBookActivity extends AppCompatActivity {
     EditText edName, edContact, edAddress;
-    Button btnBooking, btnCurLocation;
+    Button btnBooking, btnCurLocation, btnClearLocation;
 
-    TextView locLable;
+    TextView locLable, lblMedCount, lblTotalPrice;
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -25,19 +27,33 @@ public class BuyMedicineBookActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_buy_medicine_book);
 
+        Database db = new Database(getApplicationContext());
+
         edName = findViewById(R.id.editTextFullName);
         edAddress = findViewById(R.id.editTextAddress);
         edContact = findViewById(R.id.editTextContact);
         btnBooking = findViewById(R.id.buttonBMBBooking);
         locLable = findViewById(R.id.textViewLocationLable);
         btnCurLocation = findViewById(R.id.btnCurrentLocation);
+        btnClearLocation = findViewById(R.id.btnClearLocation);
+        lblMedCount = findViewById(R.id.textViewMedCount);
+        lblTotalPrice = findViewById(R.id.textViewTotalAmount);
+
+
+        int itemCount = db.getItemCountCart();
+
+        lblMedCount.setText("Medicine Count : " + itemCount);
+        lblTotalPrice.setText("Total Amount : " + AppGlobal.totalCartAmount);
+
 
 
         if (AppGlobal.userLatitude == 0 && AppGlobal.userLongitude == 0) {
             locLable.setText("Location is not set");
+            btnClearLocation.setEnabled(false);
+
         } else {
             locLable.setText("Location is set");
-
+            btnClearLocation.setEnabled(true);
         }
 
         edName.setText(AppGlobal.fullName);
@@ -56,14 +72,35 @@ public class BuyMedicineBookActivity extends AppCompatActivity {
             }
         });
 
+
+        edAddress.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+                if(edAddress.getText().toString().trim().isEmpty()){
+                    btnCurLocation.setEnabled(true);
+                }
+                else {
+                    btnCurLocation.setEnabled(false);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+
         btnCurLocation.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-
-                if (!edAddress.getText().toString().isEmpty()) {
-                    Toast.makeText(getApplicationContext(), "Address must be empty to select current location", Toast.LENGTH_SHORT).show();
-                } else {
                     if (!edName.getText().toString().isEmpty() && !edContact.getText().toString().isEmpty()) {
                         AppGlobal.fullName = edName.getText().toString();
                         AppGlobal.contactNumber = edContact.getText().toString();
@@ -72,6 +109,16 @@ public class BuyMedicineBookActivity extends AppCompatActivity {
                     finish();
                     startActivity(new Intent(BuyMedicineBookActivity.this, MapsActivity.class));
                 }
+
+        });
+
+        btnClearLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                locLable.setText("Location is not set");
+                AppGlobal.userLongitude = 0;
+                AppGlobal.userLatitude = 0;
+                btnClearLocation.setEnabled(false);
             }
         });
 
